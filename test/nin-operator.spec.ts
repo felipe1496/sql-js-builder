22cd3c56632a698312c999f0dc212b6293a53c51
felipe1496 <felipe.oliveira1496@gmail.com>
2025-08-26 10:43:21 -0300

    adiciona testes para operador nin

---
+++ test/nin-operator.spec.ts
@@ -0,0 +1,76 @@
+import { where } from "../src";
+
+describe("Tests not in operator", () => {
+  test("Regular not in condition", () => {
+    const { sql, values } = where().and("age", "nin", [40, 32, 80]).build();
+
+    expect(sql).toBe('1 = 1 AND "age" NOT IN (?, ?, ?)');
+    expect(values).toEqual([40, 32, 80]);
+  });
+
+  test("Multiple not in condition on the same condition", () => {
+    const { sql, values } = where()
+      .and("age", "nin", [40, 32, 80])
+      .and("email", "nin", ["john@doe.com"])
+      .and("rank", "nin", ["gold", "silver"])
+      .build();
+
+    expect(sql).toBe(
+      '1 = 1 AND "age" NOT IN (?, ?, ?) AND "email" NOT IN (?) AND "rank" NOT IN (?, ?)'
+    );
+    expect(values).toEqual([40, 32, 80, "john@doe.com", "gold", "silver"]);
+  });
+
+  test("Test not in or conditions", () => {
+    const { sql, values } = where()
+      .or([
+        ["age", "nin", [40, 32, 80]],
+        ["email", "nin", ["john@doe.com"]],
+      ])
+      .build();
+    expect(sql).toBe(
+      '1 = 1 AND ("age" NOT IN (?, ?, ?) OR "email" NOT IN (?))'
+    );
+    expect(values).toEqual([40, 32, 80, "john@doe.com"]);
+  });
+
+  test("Tests or and not in and conditions on the same search", () => {
+    const { sql, values } = where()
+      .and("ranking", "nin", [10, 9, 8])
+      .or([
+        ["age", "nin", [40, 32, 80]],
+        ["email", "nin", ["john@doe.com"]],
+      ])
+      .and("email", "nin", ["john@doe.com"])
+      .build();
+    expect(sql).toBe(
+      '1 = 1 AND "ranking" NOT IN (?, ?, ?) AND ("age" NOT IN (?, ?, ?) OR "email" NOT IN (?)) AND "email" NOT IN (?)'
+    );
+    expect(values).toEqual([
+      10,
+      9,
+      8,
+      40,
+      32,
+      80,
+      "john@doe.com",
+      "john@doe.com",
+    ]);
+  });
+
+  test("Error on pass number as value", () => {
+    expect(() => where().and("age", "nin", 23).build()).toThrow(Error);
+  });
+
+  test("Error on pass string as value", () => {
+    expect(() => where().and("age", "nin", "23").build()).toThrow(Error);
+  });
+
+  test("Error on pass true as value", () => {
+    expect(() => where().and("age", "nin", true).build()).toThrow(Error);
+  });
+
+  test("Error on pass null as value", () => {
+    expect(() => where().and("age", "nin", null).build()).toThrow(Error);
+  });
+});
