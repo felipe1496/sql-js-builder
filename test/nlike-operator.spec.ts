5a5db4a2c95c3801ba231b0c8be69e7b1aa215a2
felipe1496 <felipe.oliveira1496@gmail.com>
2025-08-26 11:12:12 -0300

    cria testes para nlike

---
+++ test/nlike-operator.spec.ts
@@ -0,0 +1,66 @@
+import { where } from "../src";
+
+describe("Tests nlike operator", () => {
+  test("Regular nlike value", () => {
+    const { sql, values } = where().and("name", "nlike", "john").build();
+
+    expect(sql).toBe(`1 = 1 AND upper("name") NOT LIKE upper(?)`);
+    expect(values).toEqual(["%john%"]);
+  });
+
+  test("Multiple nlike condition on the same condition", () => {
+    const { sql, values } = where()
+      .and("name", "nlike", "john")
+      .and("email", "nlike", "john@d")
+      .build();
+
+    expect(sql).toBe(
+      '1 = 1 AND upper("name") NOT LIKE upper(?) AND upper("email") NOT LIKE upper(?)'
+    );
+    expect(values).toEqual(["%john%", "%john@d%"]);
+  });
+
+  test("Test nlike or conditions", () => {
+    const { sql, values } = where()
+      .or([
+        ["name", "nlike", "john"],
+        ["name", "nlike", "doe"],
+      ])
+      .build();
+    expect(sql).toBe(
+      '1 = 1 AND (upper("name") NOT LIKE upper(?) OR upper("name") NOT LIKE upper(?))'
+    );
+    expect(values).toEqual(["%john%", "%doe%"]);
+  });
+
+  test("Tests or and and eq conditions on the same search", () => {
+    const { sql, values } = where()
+      .and("age", "nlike", "32")
+      .or([
+        ["name", "nlike", "john"],
+        ["name", "nlike", "doe"],
+      ])
+      .and("email", "nlike", "john@doe")
+      .build();
+    expect(sql).toBe(
+      '1 = 1 AND upper("age") NOT LIKE upper(?) AND (upper("name") NOT LIKE upper(?) OR upper("name") NOT LIKE upper(?)) AND upper("email") NOT LIKE upper(?)'
+    );
+    expect(values).toEqual(["%32%", "%john%", "%doe%", "%john@doe%"]);
+  });
+
+  test("Error on pass array as value", () => {
+    expect(() => where().and("age", "nlike", ["23"]).build()).toThrow(Error);
+  });
+
+  test("Error on pass number as value", () => {
+    expect(() => where().and("age", "in", 23).build()).toThrow(Error);
+  });
+
+  test("Error on pass true as value", () => {
+    expect(() => where().and("age", "in", true).build()).toThrow(Error);
+  });
+
+  test("Error on pass null as value", () => {
+    expect(() => where().and("age", "in", null).build()).toThrow(Error);
+  });
+});
